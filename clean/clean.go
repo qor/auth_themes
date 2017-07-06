487cf6385b083c80d81605a71df3bdf52adf01f8
Jinzhu <wosmvp@gmail.com>
2017-07-06 16:55:41 +0800

    Check password confirmation

--- clean/clean.go
+++ clean/clean.go
@@ -1,13 +1,29 @@
 package clean
 
 import (
+	"errors"
+
 	"github.com/qor/auth"
+	"github.com/qor/auth/claims"
 	"github.com/qor/auth/database"
 )
 
-// New initialize clean them
-func New(auth *auth.Auth) {
-	auth.RegisterProvider(database.New(nil))
+// ErrPasswordConfirmationNotMatch password confirmation not match error
+var ErrPasswordConfirmationNotMatch = errors.New("password confirmation doesn't match password")
 
-	auth.Render.PrependViewPath("github.com/qor/auth_themes/clean/views")
+// New initialize clean them
+func New(Auth *auth.Auth) {
+	Auth.RegisterProvider(database.New(&database.Config{
+		RegisterHandler: func(context *auth.Context) (*claims.Claims, error) {
+			context.Request.ParseForm()
+
+			if context.Request.Form.Get("confirm_password") != context.Request.Form.Get("password") {
+				return nil, ErrPasswordConfirmationNotMatch
+			}
+
+			return database.DefaultRegisterHandler(context)
+		},
+	}))
+
+	Auth.Render.PrependViewPath("github.com/qor/auth_themes/clean/views")
 }
