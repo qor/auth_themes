b6e1bacbec803d5e4dea2bda9ca044ea1988a02c
Jinzhu <wosmvp@gmail.com>
2017-07-07 10:07:44 +0800

    initialize clean theme

--- clean/clean.go
+++ clean/clean.go
@@ -11,8 +11,15 @@ import (
 // ErrPasswordConfirmationNotMatch password confirmation not match error
 var ErrPasswordConfirmationNotMatch = errors.New("password confirmation doesn't match password")
 
-// New initialize clean them
-func New(Auth *auth.Auth) {
+// New initialize clean theme
+func New(config *auth.Config) *auth.Auth {
+	if config == nil {
+		config = &auth.Config{}
+	}
+	config.ViewPaths = append(config.ViewPaths, "github.com/qor/auth_themes/clean/views")
+
+	Auth := auth.New(config)
+
 	Auth.RegisterProvider(database.New(&database.Config{
 		RegisterHandler: func(context *auth.Context) (*claims.Claims, error) {
 			context.Request.ParseForm()
@@ -25,5 +32,5 @@ func New(Auth *auth.Auth) {
 		},
 	}))
 
-	Auth.Render.PrependViewPath("github.com/qor/auth_themes/clean/views")
+	return Auth
 }
